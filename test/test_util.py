d782b21cbdce33d3276a88d8de169ae6cf77d531
Jeroen Vermeulen <jtvjtv@gmail.com>
2018-05-19 00:39:33 +0200

    Test identity.

--- test/test_util.py
+++ test/test_util.py
@@ -13,7 +13,7 @@ from unittest import TestCase
 
 from ..ijssel.util import (
     bind_kwargs,
-#    identity,
+    identity,
 #    ifilter,
 #    imap,
 #    negate,
@@ -36,3 +36,10 @@ class TestBindKwargs(TestCase):
         self.assertEqual(
             times(arg),
             arg * factor)
+
+
+class TestIdentity(TestCase):
+    """Tests for `identity`."""
+    def test_returns_argument(self):
+        arg = randint(0, 100)
+        self.assertEqual(identity(arg), arg)
