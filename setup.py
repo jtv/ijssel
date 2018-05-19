1b53f4c69041dfbb75b70b652e4dd779974a4cb0
Jeroen Vermeulen <jtvjtv@gmail.com>
2018-05-19 20:57:08 +0200

    Move version into VERSION file.

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 """Setup script for IJssel."""
+import codecs
 from distutils.core import setup
+import os.path
 
-setup(name='ijssel', version='1.0', modules=['ijssel'])
+
+def main():
+    here = os.path.dirname(os.path.abspath(__file__))
+    version_path = os.path.join(here, 'VERSION')
+
+    with codecs.open(version_path, encoding='utf-8') as version_file:
+        version = version_file.read().strip()
+
+    setup(
+        name='ijssel', version=version, modules=['ijssel'],
+        url='https://github.com/jtv/ijssel',
+        data_files=[(here, ['VERSION'])])
+
+
+main()
